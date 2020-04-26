19ced95b75927e31c5e826ff2fe80dc3e0b415b2
Islam <islamabdukarimov1999@mail.ru>
2020-04-27 01:24:52 +0600

    finish

--- recruting/recruting/main/models.py
+++ recruting/recruting/main/models.py
@@ -1,9 +1,28 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser, AbstractUser
 
 from recruting.skills.models import Position, SkillSet, Category
 
 from rest_framework import serializers
+
+
+MAX_FILE_SIZE = 1024000
+ALLOWED_EXTENSIONS = ['.jpg', '.png', '.doc', '.pdf']
+
+
+def validate_file_size(value):
+    if value.size > MAX_FILE_SIZE:
+        raise serializers.ValidationError(f'max file size is: {MAX_FILE_SIZE}')
+
+
+def validate_extension(value):
+    split_ext = os.path.splitext(value.name)
+    if len(split_ext) > 1:
+        ext = split_ext[1]
+        if not ext.lower() in ALLOWED_EXTENSIONS:
+            raise serializers.ValidationError(f'not allowed file, valid extensions: {ALLOWED_EXTENSIONS}')
 
 
 def validate_role(value):
@@ -29,7 +48,8 @@ class MyUser(AbstractUser):
 
 class Admin(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='admin_photo', blank=True, null=True)
+    photo = models.ImageField(upload_to='admin_photo', validators=[validate_extension, validate_file_size],
+                              blank=True, null=True)
 
     def __str__(self):
         return f'{self.id} {self.user}'
@@ -38,7 +58,9 @@ class Admin(models.Model):
 class Manager(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
     department = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
-    photo = models.ImageField(upload_to='manager_photo', blank=True, null=True)
+    photo = models.ImageField(upload_to='manager_photo', validators=[validate_extension, validate_file_size],
+                              blank=True, null=True)
+
     def __str__(self):
         return f'Manager: {self.id} {self.user} {self.department}'
 
@@ -46,7 +68,8 @@ class Manager(models.Model):
 class Employee(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
     position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name='positions', blank=True, null=True)
-    photo = models.ImageField(upload_to='employee_photo', blank=True, null=True)
+    photo = models.ImageField(upload_to='employee_photo', validators=[validate_extension, validate_file_size],
+                              blank=True, null=True)
 
     class Meta:
         verbose_name = 'Employee'
--- recruting/recruting/message/serializers.py
+++ recruting/recruting/message/serializers.py
@@ -3,14 +3,23 @@ from rest_framework import serializers
 from recruting.message.models import Chat
 
 
-class ChatSerializer(serializers.ModelSerializer):
-    from_mes = serializers.CharField(required=True)
+class ChatCreateSerializer(serializers.Serializer):
+    from_mes = serializers.CharField(required=False)
     to_mes = serializers.CharField(required=True)
     mess = serializers.CharField(required=True)
 
-    class Meta:
-        model = Chat
-        fields = ('id', 'from_mes', 'to_mes', 'mess', 'date')
-
     def create(self, validated_data):
         return Chat.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        instance.save()
+        return instance
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = '__all__'
