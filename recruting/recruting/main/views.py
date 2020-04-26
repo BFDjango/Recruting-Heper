a65bd6783221caa8561f9378c358ba075b419698
Islam <islamabdukarimov1999@mail.ru>
2020-04-26 20:19:16 +0600

    95%comlete

--- recruting/recruting/main/views.py
+++ recruting/recruting/main/views.py
@@ -10,8 +10,9 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from recruting.main.models import MyUser
-from recruting.main.serializers import MyUserFullSerializer
+from recruting.main.models import MyUser, Admin, Manager, Employee
+from recruting.main.serializers import MyUserFullSerializer, AdminSerializer, ManagerSerializer, EmployeeSerializer
+
 logger = logging.getLogger('log')
 
 
@@ -49,3 +50,83 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     def me(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
+
+
+class AdminView(generics.ListAPIView):
+    serializer_class = AdminSerializer
+    queryset = Admin.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ManagerView(generics.ListAPIView):
+    serializer_class = ManagerSerializer
+    queryset = Manager.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if request.user.role == 1:
+                return self.list(request, *args, **kwargs)
+            else:
+                return Response('Permission denied!!!')
+        except Exception as e:
+            print(e)
+            return Response('Please Authorized!!!')
+
+
+class ManagerDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ManagerSerializer
+    queryset = Manager.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if request.user.role == 1:
+                return self.retrieve(request, *args, **kwargs)
+            else:
+                return Response('Permission denied!!!')
+        except Exception as e:
+            print(e)
+            return Response('Please Authorized!!!')
+
+    def put(self, request, *args, **kwargs):
+        try:
+            if request.user.role == 1:
+                return self.update(request, *args, **kwargs)
+            else:
+                return Response('Permission denied!!!')
+        except Exception as e:
+            print(e)
+            return Response('Please Authorized!!!')
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            if request.user.role == 1:
+                return self.destroy(request, *args, **kwargs)
+            else:
+                return Response('Permission denied!!!')
+        except Exception as e:
+            print(e)
+            return Response('Please Authorized!!!')
+
+
+class EmployeeView(generics.ListAPIView):
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if request.user.role == 1:
+                return self.list(request, *args, **kwargs)
+            elif request.user.role == 2:
+                print('***********')
+                manager = Manager.objects.get(user=request.user)
+                print(manager.department)
+                empl = Employee.objects.filter(position__department=manager.department)
+                serializer = EmployeeSerializer(empl,many=True)
+                return Response(serializer.data)
+            else:
+                return Response('Permission denied!!!')
+        except Exception as e:
+            print(e)
+            return Response('Please Authorized!!!')
